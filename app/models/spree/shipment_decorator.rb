8526f996ee024a082c8ad050d66201125579d0df
adhik.mehta@digitalairtech.com <adhik.mehta@digitalairtech.com>
2021-10-11 11:55:16 +0530

    Chaging package shipping rates

---
+++ app/models/spree/shipment_decorator.rb
@@ -0,0 +1,69 @@
+module Spree
+  module ShipmentDecorator
+  	def update_amounts
+      if selected_shipping_rate
+        require 'shippo'
+        require 'awesome_print'
+
+        Shippo::API.token = '8e4c4e6b24208336bef5e321446430dd24c8e69d'
+
+        $shipment_rate = nil
+        for item in line_items do
+	      params   = {  async:          false,
+	                    address_from:   {
+	                      name:           stock_location.name,
+	                      company:        stock_location.admin_name,
+	                      street1:        stock_location.address1,
+	                      street2:        stock_location.address2,
+	                      city:           stock_location.city,
+	                      state:          stock_location.state.abbr,
+	                      zip:            stock_location.zipcode,
+	                      country:        stock_location.country.iso,
+	                      phone:          stock_location.phone },
+	                    address_to:     {
+	                      name:           address.firstname,
+	                      company:        address.company,
+	                      street1:        address.address1,
+	                      city:           address.city,
+	                      state:          address.state.abbr,
+	                      zip:            address.zipcode,
+	                      country:        address.country.iso,
+	                      phone:          address.phone,
+	                      email:          order.email },
+	                    parcels:         {
+	                      length:        item.variant.depth,
+	                      width:         item.variant.width,
+	                      height:        item.variant.height,
+	                      distance_unit: :in,
+	                      weight:        item.variant.weight,
+	                      mass_unit:     :lb }
+	                  } 
+
+	       @shipment = Shippo::Shipment.create(params)
+  	         for rate in @shipment.rates do
+	           if rate.attributes.present?
+	             if rate.attributes.first == "BESTVALUE"
+	           	   $shipment_rate = $shipment_rate.to_f + rate.amount.to_f
+	               break
+	             end
+	           end
+	         end
+
+        end 
+
+        unless $shipment_rate.nil? 
+          update_columns(
+		           cost: $shipment_rate,
+		           adjustment_total: adjustments.additional.map(&:update!).compact.sum,
+		           updated_at: Time.current
+		          )
+
+   		end
+
+        
+      end
+    end
+
+   Spree::Shipment.prepend Spree::ShipmentDecorator
+  end
+end
---
+++ lib/spree_goshippo.rb
@@ -0,0 +1,4 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_goshippo/engine'
+require 'spree_goshippo/version'
